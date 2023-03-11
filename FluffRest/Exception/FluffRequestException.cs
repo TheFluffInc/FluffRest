105519677841ca28ee9e8e1aed2ab8fa765ac908
Asmoyed <toutaingu@gmail.com>
2023-03-11 21:36:50 +0100

    Add string exec, custom serialization, and better exceptions handling

--- FluffRest/Exception/FluffRequestException.cs
+++ FluffRest/Exception/FluffRequestException.cs
@@ -1,13 +1,31 @@
-﻿namespace FluffRest.Exception
+﻿using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace FluffRest.Exception
 {
     public class FluffRequestException : System.Exception
     {
+        public HttpStatusCode StatusCode { get; private set; }
         public string Content { get; private set; }
 
-        public FluffRequestException(string message, string content, System.Exception inner)
+        public FluffRequestException(string message, string content, HttpStatusCode httpStatusCode, System.Exception inner)
             : base (message, inner)
         {
             Content = content;
+            StatusCode = httpStatusCode;
+        }
+
+        public ValueTask<T> DeserializeAsync<T>(CancellationToken cancellationToken = default)
+        {
+            byte[] contentBytes = Encoding.UTF8.GetBytes(Content);
+            MemoryStream stream = new MemoryStream(contentBytes);
+            var jsonWebOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+            return JsonSerializer.DeserializeAsync<T>(stream, jsonWebOptions, cancellationToken: cancellationToken);
         }
     }
 }
