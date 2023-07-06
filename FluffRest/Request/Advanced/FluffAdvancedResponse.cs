59737e6fa7758623f4a6d37b89408290358e9905
Sethexus <sethexus@outlook.fr>
2023-07-06 22:16:40 +0200

    Add advanced request

---
+++ FluffRest/Request/Advanced/FluffAdvancedResponse.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+
+namespace FluffRest.Request.Advanced
+{
+    public sealed class FluffAdvancedResponse<T>
+    {
+        /// <summary>
+        /// Deserialized content from response.
+        /// </summary>
+        public T Content { get; private set; }
+
+        /// <summary>
+        /// Status code from response.
+        /// </summary>
+        public HttpStatusCode StatusCode { get; private set; }
+
+        internal FluffAdvancedResponse(T content, HttpStatusCode statusCode)
+        {
+            Content = content;
+            StatusCode = statusCode;
+        }
+    }
+}
